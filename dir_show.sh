37de7b1af85daed851354561d5499cc3d665e70a
sclgcj <sclgcj2@163.com>
2018-06-06 11:23:48 +0800

    增加dir_show.sh文件

---
+++ dir_show.sh
@@ -0,0 +1,28 @@
+#/bin/bash
+
+
+if [ "$1" == "" ]
+then
+	exit
+fi
+
+date 
+dirs=`find . | grep $1`
+date
+for d in $dirs:
+do
+	if [ -d $d ];
+	then
+		d2=${d##*/}
+		if [[ "$d2" =~ "$1" ]]
+		then
+			echo -ne "$d (yes/no)"
+			read choice
+			if [ "$choice" == "yes" ] || [ "$choice" == "y" ]
+			then
+				cd $d
+				break;
+			fi
+		fi
+	fi
+done
