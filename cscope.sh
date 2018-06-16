3a5cbf37ece616f2f7884c3eba5f62cd4a76d6c8
sclgcj <sclgcj2@163.com>
2018-06-16 14:58:30 +0800

    取消错误信息输出

--- cscope.sh
+++ cscope.sh
@@ -1,5 +1,5 @@
 find `pwd` -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
 
-cscope -bkq -i cscope.files
+cscope -bkq -i cscope.files 2>/dev/null
 
-ctags -R  --C-kinds=+p --fields=+aS --extra=+q
+ctags -R  --C-kinds=+p --fields=+aS --extra=+q 2>/dev/null
