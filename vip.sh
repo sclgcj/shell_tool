06c5f468eb8b3662b3635fc69d62317df87879ed
sclgcj <sclgcj2@163.com>
2021-06-25 10:25:51 +0800

    Update vip.sh

--- vip.sh
+++ vip.sh
@@ -26,11 +26,48 @@ else
 	cur_path=`pwd`
 	lines=`cat $proj_conf`
 	echo "lines = $lines"
+	
+	a="let g:syntastic_c_include_dirs = ["
+	b=0
 	for i in $lines;
 	do
 		echo "i = $i"
+		if [ "$i" == "[ctrl]" ];then
+			ctrl=1
+			echo"ctrl = $ctrl"
+		elif [ "$i" == "[dir]" ];then
+			ctrl=0;
+			continue;
+		fi
+		if [ $ctrl -eq 1 ];then
+			if [ "$i" == "keep" ];then
+				keep=1
+				data=`cat .cscope.keep 2>/dev/null | tr '\n' '\0'`
+				if [ "$data" == "true" ];then
+					update=0
+					break
+				fi
+			fi
+		fi
 		if [ -d $cur_path/$i ];then
 			cd $cur_path/$i;
+			for line in `find . -name \*.h | xargs dirname 2>/dev/null | uniq`;
+			do
+				if [ $b -eq 0 ];then
+					a=$a"\"$cur_path/$i$line\""
+					b=1
+				else
+					a=$a",\"$cur_path/$i$line\""
+				fi
+			done
+			cur_inc=`ls *.h 2>/dev/null`
+			if [ "$cur_inc" != "" ];then
+				if [ $b -eq 0 ];then
+					a=$a"\"$cur_path/$i\""
+				else
+					a=$a",\"$cur_path/$i\""
+				fi
+			fi
 			#if [ ! -e tags ];then
 			echo "prepare ctags for $cur_path/$i"
 			ctags -R  --C-kinds=+p --fields=+aS --extra=+q 2>/dev/null
@@ -45,7 +82,32 @@ else
 			cd - >/dev/null
 		fi
 	done
-	cscope.sh $proj_conf
+	if [ -e ./.extra_inc ];then
+		b=0
+		extra_lines=`cat ./.extra_inc`
+		for extra_i in $extra_lines;
+		do
+			if [ $b -eq 0 ];then
+				a=$a"\$extra_i\"
+				b=1
+			else
+				a=$a",\"extra_i\""
+			fi
+		done
+	fi
+	a=$a"]"
+	exho "a=$a"
+	echo "update = $update"
+	if [ $update -eq 1 ];then
+		echo $a >> $vim_file
+		cscope.sh $proj_conf
+	fi
+fi
+
+if [ $keep -eq 1 ];then
+	echo "true" > .cscope.keep
+else
+	echo "false" > .cscope.keep
 fi
 
 cscope_auto.sh `pwd` $proj_conf >/dev/null 2>&1 &
